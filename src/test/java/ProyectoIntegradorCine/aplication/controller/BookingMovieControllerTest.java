bbdda23b11562f0c0f363fd47f6da9ddb81a30c9
jhonsbaracaldo <jhonsbaracaldo@gmail.com>
2024-02-13 21:42:40 -0500

    creando configuracion con mockito y creando la configuracion la que se va a realizar la prueba

--- src/test/java/ProyectoIntegradorCine/aplication/controller/BookingMovieControllerTest.java
+++ src/test/java/ProyectoIntegradorCine/aplication/controller/BookingMovieControllerTest.java
@@ -1,14 +1,28 @@
 package ProyectoIntegradorCine.aplication.controller;
 
+import ProyectoIntegradorCine.aplication.services.BookingMovieServices;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BookingMovieControllerTest {
+// Anotacion para realizar las pruebas de integracion
+   private MockMvc mockMvc;
+// @Mock para simular mi services
+   @Mock
+   private BookingMovieServices bookingMovieServices;
+// creamos la configuracion necesaria para la pruebas  en Septup
+   @InjectMocks
+   private BookingMovieController bookingMovieController;
 
     @BeforeEach
-    void setUp() {
+    public void setup() {
+        mockMvc = MockMvcBuilders.standaloneSetup(bookingMovieController).build();
     }
 
     @Test
