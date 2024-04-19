9460274f6d820d8d4ad884f509b4c6fc2ab3d422
jhonsbaracaldo <jhonsbaracaldo@gmail.com>
2024-04-19 17:00:24 -0500

    se comidifica archivo yml para despliegue

--- src/main/java/ProyectoIntegradorCine/ProyectoIntegradorCineApplication.java
+++ src/main/java/ProyectoIntegradorCine/ProyectoIntegradorCineApplication.java
@@ -2,8 +2,12 @@ package ProyectoIntegradorCine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.awt.*;
 import java.util.List;
@@ -16,6 +20,18 @@ public class ProyectoIntegradorCineApplication {
 		SpringApplication.run(ProyectoIntegradorCineApplication.class, args);
 	}
 
-
+	@Configuration
+	public static class Myconfiguration{
+		@Bean
+		public WebMvcConfigurer corsConfigurer(){
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+				}
+			};
+		}
+	}
 
 }
